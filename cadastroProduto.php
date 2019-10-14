0b216e5c36bf3a3a59008a9193616210dbe68659
Monica Craveiro <craveiromonica@gmail.com>
2019-10-14 12:10:12 -0300

    Cadastrando produtos em JSON e salvando img

--- cadastroProduto.php
+++ cadastroProduto.php
@@ -2,15 +2,42 @@
     function cadastrarProduto($nomeProduto,$descProduto,$imgProduto,$precoProduto){
         $nomeArquivo = "produto.json";
         if(file_exists($nomeArquivo)){
+            $arquivo = file_get_contents($nomeArquivo);
+            $produtos = json_decode($arquivo,true);
+            $produtos[] = ["nome"=>$nomeProduto,"preco"=>$precoProduto,"desc"=>$descProduto,"img"=>$imgProduto];
+            var_dump($produtos);
+            $json = json_encode($produtos);
+            $deuCerto = file_put_contents($nomeArquivo,$json);
 
+            if($deuCerto){
+                return "Deu Bom!";
+            }else{
+                return "Deu ruim!";
+            }
         }else{
             $produtos = [];
             $produtos[] = ["nome"=>$nomeProduto,"preco"=>$precoProduto,"desc"=>$descProduto,"img"=>$imgProduto];
-            var_dump($produtos);
+            // var_dump($produtos);
+            $json = json_encode($produtos);
+            $deuCerto = file_put_contents($nomeArquivo,$json);
+
+            if($deuCerto){
+                return "Deu Bom!";
+            }else{
+                return "Deu ruim!";
+            }
         }
     }
     if($_POST){
-        cadastrarProduto($_POST["nomeProduto"],$_POST["descProduto"],$_POST["imgProduto"],$_POST["precoProduto"]);
+        // var_dump($_FILES);
+        // exit;
+        $nomeImg = $_FILES["imgProduto"]["name"];
+        $locasTmp = $_FILES["imgProduto"]["tmp_name"];
+        $caminhoSalvo = "img/".$nomeImg;
+
+        $deuCerto = move_uploaded_file($locasTmp,$caminhoSalvo);
+        exit;
+        echo cadastrarProduto($_POST["nomeProduto"],$_POST["descProduto"],$_POST["imgProduto"],$_POST["precoProduto"]);
     }
 ?>
 
@@ -20,8 +47,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Cadastro Produto</title>
 </head>
 <body>
     <?php include_once("header.php"); ?>
@@ -31,18 +58,18 @@
                 <h1>Cadastro de Produto</h1>
             </div>
             <div class="col-12">
-                <form action="" method="post">
+                <form action="" method="post" enctype="multipart/form-data">
                     <div class="form-group">
-                        <input type="text" class="form-control" name="nomeProduto" placehorder="Nome do Produto">
+                        <input type="text" class="form-control" name="nomeProduto" placeholder="Nome do Produto"/>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="descProduto" placehorder="Descrição do Produto">
+                        <input type="text" class="form-control" name="descProduto" placeholder="Descrição do Produto"/>
                     </div>
                     <div class="form-group">
-                        <input type="file" class="form-control" name="imgProduto" placehorder="Imagem do Produto">
+                        <input type="file" class="form-control" name="imgProduto" placeholder="Imagem do Produto"/>
                     </div>
                     <div class="form-group">
-                        <input type="number" class="form-control" name="precoProduto" placehorder="Preço do Produto">
+                        <input type="number" class="form-control" name="precoProduto" placeholder="Preço do Produto"/>
                     </div>
                     <button class="btn btn-success">Cadastrar Produto</button>
                 </form>
